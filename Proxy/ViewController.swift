1c29e8d55ef020a4e479650deedc0ee24b6c11b3
Sergei Poluboiarinov <sihtmark@icloud.com>
2022-11-27 10:31:48 +0300

    Initial Commit

---
+++ Proxy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Proxy
+//
+//  Created by Sergei Poluboiarinov on 2022-11-27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
